c13e94d7d7fd84fb085d9aa4dbcc33b0373db0b2
alaelbenna <elbenna.ala@outlook.com>
2021-10-22 12:46:56 -0700

    command line arguments in argv array

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: The number of command line arguments in argv array
+ * @argv: An array containing the program command line arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
